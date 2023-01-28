543e5e468812c19f83b07520b338b4dfef0fc373
Muhmmad Imtinan Farooq <92246555+alidesu@users.noreply.github.com>
2023-01-28 10:59:31 +0500

    new update

--- Entering elements with For-Loop in Arrays.cpp
+++ Entering elements with For-Loop in Arrays.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 int main() {
 
-
-
+    int arry[10] {};
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Enter the Value no."<<i<<": ";
+        cin>>arry[i];
+    }
+     
+    
     
     return 0;
 }
